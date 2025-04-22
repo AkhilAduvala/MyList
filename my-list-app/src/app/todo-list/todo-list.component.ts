1edbd2d58f6b6b24339e5e5c83eac57583a07374
AKHIL ADUVALA <aduvalaakhil@gmail.com>
2025-04-22 22:57:40 +0530

    Day 1: Session 2:
    @Input() to pass data from one component to the other component

---
+++ my-list-app/src/app/todo-list/todo-list.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { ToDo } from '../models/todo.model';
+import { TodoItemComponent } from '../todo-item/todo-item.component';
+
+@Component({
+  selector: 'app-todo-list',
+  imports: [TodoItemComponent],
+  templateUrl: './todo-list.component.html',
+  styleUrl: './todo-list.component.css'
+})
+
+export class TodoListComponent {
+
+  toDoQueue: ToDo[] = [
+    {id: 1, title: "Morning Code", status: "Completed"},
+    {id: 2, title: "Brunch Learning", status: "In Progress"},
+    {id: 3, title: "Lunch Code", status: "Yet To Start"},
+    {id: 4, title: "Evening Learning", status: "Yet To Start"},
+    {id: 5, title: "Dinner Code", status: "Yet To Start"},
+    {id: 6, title: "Night Learning", status: "Yet To Start"}
+  ];
+}
